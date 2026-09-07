bf5202d8834d28a8faf2abadec77742ef5d60753
phbretier <133363019+phbretier@users.noreply.github.com>
2026-09-07 14:51:46 +0200

    Velivol HS

--- script.js
+++ script.js
@@ -46,8 +46,8 @@ function celluleMeteo(colspan, cheminMeteociel, nom, latitude, longitude) {
             <div style="height: 10px;"></div>
             <a href="${urlWindy}" target="_blank" class="big">📈</a>
             &hairsp;
-            <a href="${urlVelivole}" target="_blank" class="big">📊</a>
-            &hairsp;
+            // <a href="${urlVelivole}" target="_blank" class="big">📊</a>
+            // &hairsp;
             <a href="${urlSondage}" target="_blank" class="big">📉</a>
             <div style="height: 10px;"></div>
         </td>
