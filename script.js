edb80989d8d2d15545d7e1d2ef5c3b4697104e9f
phbretier <133363019+phbretier@users.noreply.github.com>
2026-09-07 19:12:51 +0200

    Retour velivole

--- script.js
+++ script.js
@@ -46,10 +46,11 @@ function celluleMeteo(colspan, cheminMeteociel, nom, latitude, longitude) {
             <div style="height: 10px;"></div>
             <a href="${urlWindy}" target="_blank" class="big">📈</a>
             &hairsp;
+            <a href=u"${urlVelivole}" target="_blank" class="big">📊</a>
+            &hairsp;
             <a href="${urlSondage}" target="_blank" class="big">📉</a>
             <div style="height: 10px;"></div>
         </td>
     `;
-            // <a href="${urlVelivole}" target="_blank" class="big">📊</a>
-            // &hairsp;
+;
 }
