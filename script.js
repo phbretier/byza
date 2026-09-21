cf77d16ca2475542809e928ef4a374b59bd165eb
phbretier <133363019+phbretier@users.noreply.github.com>
2026-09-21 13:53:04 +0200

    fix test2

--- script.js
+++ script.js
@@ -31,18 +31,18 @@ function test2() {
                 container.className = 'menu-open';
 
                 const htmlContent = \`
-                  <a href="https://meteo-parapente.com/#/${this.dataset.lat},${this.dataset.lon},9" target="_blank" class="big">🪂</a>
+                  <a href="https://meteo-parapente.com/#/\${this.dataset.lat},\${this.dataset.lon},9" target="_blank" class="big">🪂</a>
                   &#8239;
-                  <a href="https://www.windy.com/${this.dataset.lat}/${this.dataset.lon}/airgram?clouds,${this.dataset.lat},${this.dataset.lon},10,i:temp,p:wind" target="_blank" class="big">⛅</a>
+                  <a href="https://www.windy.com/\${this.dataset.lat}/\${this.dataset.lon}/airgram?clouds,\${this.dataset.lat},\${this.dataset.lon},10,i:temp,p:wind" target="_blank" class="big">⛅</a>
                   &#8239;
-                  <a href="https://www.velivole.fr/profile?lat=${this.dataset.lat}&long=${this.dataset.lon}&model=AROME" target="_blank" class="big">📊</a>
+                  <a href="https://www.velivole.fr/profile?lat=\${this.dataset.lat}&long=\${this.dataset.lon}&model=AROME" target="_blank" class="big">📊</a>
                   <br>
                   <div style="height: 10px;"></div>
-                  <a href="meteoblue.html#${this.dataset.nom}/${this.dataset.lat}/${this.dataset.lon}" target="_blank" class="big">🌎</a>
+                  <a href="meteoblue.html#\${this.dataset.nom}/\${this.dataset.lat}/\${this.dataset.lon}" target="_blank" class="big">🌎</a>
                   &#8239;
-                  <a href="https://www.windy.com/plugin/sdg/aromeFrance/${this.dataset.lat}/${this.dataset.lon}?aromeFrance,clouds,${this.dataset.lat},${this.dataset.lon},10,i:pressure,p:wind" target="_blank" class="big">📈</a>
+                  <a href="https://www.windy.com/plugin/sdg/aromeFrance/\${this.dataset.lat}/\${this.dataset.lon}?aromeFrance,clouds,\${this.dataset.lat},\${this.dataset.lon},10,i:pressure,p:wind" target="_blank" class="big">📈</a>
                   &#8239;
-                  <a href="https://www.meteociel.fr/modeles/sondage2arome.php?mode=0&lon=${this.dataset.lat}&lat=${this.dataset.lon}&ech=0&map=0" target="_blank" class="big">📉</a>
+                  <a href="https://www.meteociel.fr/modeles/sondage2arome.php?mode=0&lon=\${this.dataset.lat}&lat=\${this.dataset.lon}&ech=0&map=0" target="_blank" class="big">📉</a>
                   <div style="height: 10px;"></div>
                 \`;
                 // Insérer le contenu HTML dans le container
