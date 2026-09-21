daf0798761fc56c535f42b8bc3179307b1a02549
phbretier <133363019+phbretier@users.noreply.github.com>
2026-09-21 17:04:48 +0200

    invert vv

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ function previsions(colspan, cheminMeteociel, nom, lat, lon) {
             &#8239;
             <a href="${urlWindyAirgram}" target="_blank" class="big"><img src="https://www.windy.com/favicon.ico" alt="⛅" width="24px"/></a>
             &#8239;
-            <a href="${urlVelivole}" target="_blank" class="big"><img src="${iconVV}" alt="📊" width="24px"/></a>
+            <a href="${urlVelivole}" target="_blank" class="big"><img src="${iconVV}" style="filter: invert(1);" alt="📊" width="24px"/></a>
             <br>
             <div style="height: 10px;"></div>
             <a href="meteoblue.html#${encodeURIComponent(nom)}}/${lat}/${lat}" target="_blank" class="big"><img src="https://www.meteoblue.com/favicon.ico" alt="🌎" width="24px"/></a>
