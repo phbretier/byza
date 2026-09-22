ff2e4cdcd1d29af54a63e92c5559930f7ec0d419
phbretier <133363019+phbretier@users.noreply.github.com>
2026-09-22 11:07:04 +0200

    mise en page

--- script.js
+++ script.js
@@ -122,7 +122,7 @@ function BasDePage() {
             // le container à ouvrir
             const container = document.createElement('div');
             container.className = 'menu-opened';
-            container.style.fontSize = 'x-small';
+            container.style.fontSize = 'small';
             container.style.textAlign = 'left';
 
 
@@ -132,25 +132,25 @@ function BasDePage() {
                 <a href="https://meteo-parapente.com/#/${this.dataset.lat},${this.dataset.lon},9" target="_blank" class="big">
                     <img src="https://meteo-parapente.com/v5/icons/icon-180.png" alt="🪂" width="24px"/> MétéoParap.
                 </a>
-                <br><div style="height: 5px;"></div>
+                <br><div style="height: 7px;"></div>
                 <a href="https://www.windy.com/${this.dataset.lat}/${this.dataset.lon}/airgram?clouds,${this.dataset.lat},${this.dataset.lon},10,i:temp,p:wind" target="_blank" class="big">
                     <img src="https://www.windy.com/favicon.ico" alt="⛅" width="24px"/> AirGram
                 </a>
-                <br><div style="height: 5px;"></div>
+                <br><div style="height: 7px;"></div>
                 <a href="https://www.velivole.fr/profile?lat=${this.dataset.lat}&long=${this.dataset.lon}&model=AROME" target="_blank" class="big">
                     <img src="${iconVV}" style="filter: invert(1);" alt="📊" width="24px"/> Coupe
                 </a>
-                <br><div style="height: 5px;"></div>
+                <br><div style="height: 7px;"></div>
                 <a href="meteoblue.html#${this.dataset.nom}/${this.dataset.lat}/${this.dataset.lon}" target="_blank" class="big">
                     <img src="https://www.meteoblue.com/favicon.ico" alt="🌎" width="24px"/> MétéoBlue
                 </a>
-                <br><div style="height: 5px;"></div>
+                <br><div style="height: 7px;"></div>
                 <a href="https://www.windy.com/plugin/sdg/aromeFrance/${this.dataset.lat}/${this.dataset.lon}?aromeFrance,clouds,${this.dataset.lat},${this.dataset.lon},10,i:pressure,p:wind" target="_blank" class="big">
-                    <img src="https://flyxc.app/static/img/jumoplane.svg" alt="📈" width="24px"/> Emagramme
+                    <img src="https://flyxc.app/static/img/jumoplane.svg" alt="📈" width="24px"/> Émagramme
                 </a>
-                <br><div style="height: 5px;"></div>
+                <br><div style="height: 7px;"></div>
                 <a href="https://www.meteociel.fr/modeles/sondage2arome.php?mode=0&lon=${this.dataset.lat}&lat=${this.dataset.lon}&ech=0&map=0" target="_blank" class="big">
-                    <img src="https://meteofrance.fr/sites/default/files/favicon_0.png" alt="📉" width="24px"/> Emagramme
+                    <img src="https://meteofrance.fr/sites/default/files/favicon_0.png" alt="📉" width="24px"/> Émagramme
                 </a>
                 <br><div style="height: 10px;"></div>
             `;
